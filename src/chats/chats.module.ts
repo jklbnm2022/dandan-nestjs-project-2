28b6fec2c588a0ab2f4ca5dc7e13f89918d682dd
seongho <jklbnm2002@naver.com>
2022-10-24 12:06:40 +0900

    추가: mongoDB 연결

--- src/chats/chats.module.ts
+++ src/chats/chats.module.ts
@@ -1,7 +1,16 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { ChatsGateway } from './chats.gateway';
+import { Chatting, ChattingSchema } from './models/chatting.model';
+import { Socket as SocketModel, SocketSchema } from './models/sockets.model';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([
+      { name: Chatting.name, schema: ChattingSchema },
+      { name: SocketModel.name, schema: SocketSchema },
+    ]),
+  ],
   providers: [ChatsGateway],
   exports: [ChatsGateway],
 })
